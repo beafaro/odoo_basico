dea78280f6599e6ee76b15225e9dfa5545675cdc
a20beatrizfc <farocarrera.beatriz@gmail.com>
2021-11-09 10:40:43 +0100

    inform

--- models/informacion.py
+++ models/informacion.py
@@ -1,4 +1,19 @@
+# -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
-autorizado = fields.Boolean(string="¿Autorizado?", default=True)
+
+class informacion(models.Model):
+    _name = 'odoo_basico.informacion'
+    _description = 'Exemplo de odoo basico'
+
+    name = fields.Char(string="Título")
+    descripcion = fields.Text(string="A descripción")
+    alto = fields.Integer(string="Alto en centimetros")
+    largo = fields.Integer(string="Largo en centimetros")
+    ancho = fields.Integer(string="Ancho en centimetros")
+    volumen = fields.Float(string="Volumen")
+    peso = fields.Float(string="Peso en Kg's")
+    densidad = fields.Float(string="Densidad")
+    autorizado = fields.Boolean(string="¿Autorizado?", default=True)
+
