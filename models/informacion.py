d3622e5563ad4ef215365249d2c13d694a7ee9e2
beafaro <farocarrera.beatriz@gmail.com>
2022-01-25 10:32:25 +0100

    informes data e hora

--- models/informacion.py
+++ models/informacion.py
@@ -3,7 +3,9 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 from odoo.exceptions import Warning
-import os
+import os, locale
+import pytz
+from . import miñasUtilidades
 
 
 class informacion(models.Model):
@@ -18,15 +20,15 @@ class informacion(models.Model):
     longo_en_cms = fields.Integer(string="Largo en centímetros")
     ancho_en_cms = fields.Integer(string="Ancho en centímetros")
     volume = fields.Float(compute="_volume", store=True)
-    peso = fields.Float(string="Peso en Kg's", default=2.7, digits=(6,2))
+    peso = fields.Float(string="Peso en Kg's", default=2.7, digits=(6, 2))
     densidade = fields.Float(compute="_densidade", store=True)
     literal = fields.Char(store=False)
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
-    sexo_traducido = fields.Selection([("Hombre","Home"), ("Mujer","Muller"), ("Otros","Outros")], string="Sexo")
+    sexo_traducido = fields.Selection([("Hombre", "Home"), ("Mujer", "Muller"), ("Otros", "Outros")], string="Sexo")
     foto = fields.Binary(string='Foto')
     adxunto_nome = fields.Char(string="Nome Adxunto")
     adxunto = fields.Binary(string="Arquivo adxunto")
-    #Os campos Many2one crean un campo na BD
+    # Os campos Many2one crean un campo na BD
     moeda_id = fields.Many2one('res.currency', domain="[('position','=','after')]")
     # con domain, filtramos os valores mostrados. Pode ser mediante unha constante (vai entre comillas) ou unha variable
     gasto_en_moeda_seleccionada_polo_usuario = fields.Monetary("Gasto en moeda seleccionada", 'moeda_id')
@@ -34,6 +36,16 @@ class informacion(models.Model):
     moeda_euro_id = fields.Many2one('res.currency', default=lambda self: self.env['res.currency'].search([('name', '=', "EUR")], limit=1))
     creador_da_moeda = fields.Char(related="moeda_id.create_uid.login", string="Usuario creador da moeda", store=True)
     gasto_en_euros = fields.Monetary("Gasto en Euros", 'moeda_euro_id')
+
+    ''' clase día 25/01/2022'''
+    data = fields.Date(string="Data", default=lambda self: fields.Date.today())
+    data_hora = fields.Datetime(string="Data e Hora", default=lambda self: fields.Datetime.now())
+    hora_utc = fields.Char(compute="_hora_utc", string="Hora UTC", size=15, store=True)
+    hora_timezone_usuario = fields.Char(compute="_hora_timezone_usuario", string="Hora Timezone do Usuario", size=15, store=True)
+    hora_actual = fields.Char(compute="_hora_actual", string="Hora Actual", size=15, store=True)
+    mes_castelan = fields.Char(compute="_hora_actual", string="Mes Castelán", size=15, store=True)
+    mes_galego = fields.Char(compute="_hora_actual", string="Mes Galego", size=15, store=True)
+
 
     @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
     def _volume(self):
@@ -69,6 +81,68 @@ class informacion(models.Model):
         for rexistro in self:
             # Ao usar warning temos que importar a libreria mediante from odoo.exceptions import Warning
             # Importamos tamén a libreria os mediante import os
-            raise Warning('Contexto: %s Ruta: %s Contido do directorio %s' % (rexistro.env.context, os.getcwd(),os.listdir(os.getcwd())))
+            raise Warning('Contexto: %s Ruta: %s Contido do directorio %s' % (rexistro.env.context, os.getcwd(), os.listdir(os.getcwd())))
             # env.context é un diccionario  https://www.w3schools.com/python/python_dictionaries.asp
         return True
+
+    def convirte_data_hora_de_utc_a_timezone_do_usuario(self, data_hora_utc_object):  # recibe a data hora en formato object
+        usuario_timezone = pytz.timezone(self.env.user.tz or 'UTC')  # obter a zona horaria do usuario. Ollo!!! nas preferencias do usuario ten que estar ben configurada a zona horaria
+        return pytz.UTC.localize(data_hora_utc_object).astimezone(usuario_timezone)  # hora co horario do usuario en formato object
+        # para usar  pytz temos que facer  import pytz
+
+    def actualiza_hora_actual_UTC(self):  # Esta función é chamada dende un boton de informacion.xml e dende _hora_actual
+        for rexistro in self:
+            rexistro.hora_actual = fields.Datetime.now().strftime("%H:%M:%S")
+        # Grava a hora en UTC, se quixesemos poderiamos usar a función  _convirte_data_hora_de_utc_a_timezone_do_usuario
+
+    # Esta función será chamada dende a función actualiza_hora_timezone_usuario_dende_boton_e_apidepends e
+    #  dende pedido.py (Cando insertamos os valores do template self.env.user.tz non ten o timezone do usuario por iso se carga coa hora UTC,
+    #  o botón en pedido.py é para actualizar todos os rexistros masivamente dende outro modelo)
+    def actualiza_hora_timezone_usuario(self, obxeto_rexistro):
+        obxeto_rexistro.hora_timezone_usuario = self.convirte_data_hora_de_utc_a_timezone_do_usuario(obxeto_rexistro.data_hora).strftime("%H:%M:%S")
+        # Convertimos a hora de UTC a hora do timezone do usuario
+
+    @api.depends('data_hora')
+    def _hora_utc(self):
+        for rexistro in self:  # A hora se almacena na BD en horario UTC (2 horas menos no verán, 1 hora menos no inverno)
+            rexistro.hora_utc = rexistro.data_hora.strftime("%H:%M:%S")
+
+    @api.depends('data_hora')
+    def _hora_actual(self):
+        for rexistro in self:
+            rexistro.actualiza_hora_actual_UTC()
+
+    def actualiza_hora_timezone_usuario_dende_boton_e_apidepends(self):  # Esta función é chamada dende un boton de informacion.xml e dende @api.depends _hora_timezone_usuario
+        self.actualiza_hora_timezone_usuario(self)  # leva self como parametro por que actualiza_hora_timezone_usuario ten 2 parametros
+        # porque usamos tamén actualiza_hora_timezone_usuario dende outro modelo (pedido.py) e lle pasamos como parámetro o obxeto_rexistro
+
+    @api.depends('data_hora')
+    def _hora_timezone_usuario(self):
+        for rexistro in self:
+            rexistro.actualiza_hora_timezone_usuario_dende_boton_e_apidepends()
+
+    # Podemos  configurar locales a nivel de sistema con dpkg-reconfigure locales poñendo un por defecto.
+    # apt-get install locales
+    # dpkg-reconfigure locales (podemos configurar varios)
+    # locale (ver o locale por defecto)
+    # locale -a (ver os dispoñibles)
+    @api.depends('data_hora')
+    def _mes_castelan(self):
+        # O idioma por defecto é o configurado en locale na máquina onde se executa odoo.
+        # Podemos cambialo con locale.setlocale, os idiomas teñen que estar instalados na máquina onde se executa odoo.
+        # Lista onde podemos ver os distintos valores: https://docs.moodle.org/dev/Table_of_locales#Table
+        # Definimos en miñasUtilidades un método para asignar o distinto literal que ten o idioma en función da plataforma Windows ou GNULinux
+        locale.setlocale(locale.LC_TIME, miñasUtilidades.cadeaTextoSegunPlataforma('Spanish_Spain.1252', 'es_ES.utf8'))
+        for rexistro in self:
+            rexistro.mes_castelan = rexistro.data_hora.strftime("%B")  # strftime https://strftime.org/
+
+    @api.depends('data_hora')
+    def _mes_galego(self):
+        # O idioma por defecto é o configurado en locale na máquina onde se executa odoo.
+        # Podemos cambialo con locale.setlocale, os idiomas teñen que estar instalados na máquina onde se executa odoo.
+        # Lista onde podemos ver os distintos valores: https://docs.moodle.org/dev/Table_of_locales#Table
+        # Definimos en miñasUtilidades un método para asignar o distinto literal que ten o idioma en función da plataforma Windows ou GNULinux
+        locale.setlocale(locale.LC_TIME, miñasUtilidades.cadeaTextoSegunPlataforma('Galician_Spain.1252', 'gl_ES.utf8'))
+        for rexistro in self:
+            rexistro.mes_galego = rexistro.data_hora.strftime("%B")
+        locale.setlocale(locale.LC_TIME, miñasUtilidades.cadeaTextoSegunPlataforma('Spanish_Spain.1252', 'es_ES.utf8'))
---
+++ models/miñasUtilidades.py
@@ -0,0 +1,8 @@
+import os, platform
+
+def cadeaTextoSegunPlataforma(cadeaTextoWindows,cadeaTextoGNULinux):
+    if platform.system().lower() == 'windows':
+        cadeaTexto = cadeaTextoWindows
+    else:
+        cadeaTexto = cadeaTextoGNULinux
+    return cadeaTexto
